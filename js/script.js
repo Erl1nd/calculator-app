fb4c079964a4b5572a627a161791f6c0af55758f
Erlind <erlind.uxdesigner@gmail.com>
2023-05-09 17:43:35 +0800

    Added add feature and equals

--- js/script.js
+++ js/script.js
@@ -5,27 +5,69 @@ const resultOutput = document.querySelector(".result-output-screen");
 // Button
 const acBtn = document.querySelector(".ac-button");
 const deleteBtn = document.querySelector(".delete-button");
-const numberBtn = document.querySelectorAll(".number-button");
+const numberBtn = document.querySelectorAll(".number-button div");
 const addBtn = document.querySelector(".add-button");
 const subtractBtn = document.querySelector(".subtract-button");
 const multiplyBtn = document.querySelector(".multiply-button");
 const dividBtn = document.querySelector(".divide-button");
 const decimalBtn = document.querySelector(".decimal-button");
+const equalsBtn = document.querySelector(".equals-button");
 
 
+let firstNumber;
+let secondNumber;
+let operator;
+let numberContainer = []
+let answer;
 
 
-
-
-
-// AC(clear)
+// AC
 acBtn.addEventListener("click", function(){
     userInput.textContent = "";
+    resultOutput.textContent = "";
+    numberContainer = [];
+    answer = 0;
+    addBtn.removeAttribute("disabled")
 })
 
-// when clicking the number button
+// Number
 numberBtn.forEach(button => {
     button.addEventListener("click", function(){
         userInput.textContent += this.textContent;
+        resultOutput.textContent += this.textContent;
     })
 });
+
+
+
+
+
+// Add
+addBtn.addEventListener("click", function(){
+    userInput.textContent += " + ";
+    if (firstNumber == null || secondNumber == null) {
+        numberContainer.push(parseInt(resultOutput.textContent))
+        resultOutput.textContent = "";
+        if (numberContainer.length == 2) {
+            answer = numberContainer[0] + numberContainer[1];
+            numberContainer = [];
+            numberContainer.push(answer);
+        }
+    } 
+    console.log(numberContainer);
+})
+
+
+
+
+// EQUALS: give answer
+equalsBtn.addEventListener("click", function(){
+    answer = add(numberContainer[0], parseInt(resultOutput.textContent));
+    resultOutput.textContent = answer;
+    addBtn.setAttribute("disabled", "");
+})
+
+
+function add(a, b) {
+    return a + b;
+}
